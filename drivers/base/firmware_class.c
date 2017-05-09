6da540b576661b46ba3866a0e015a9c6c2d9adf0
Wig C <onlywig@gmail.com>
2017-05-09 18:04:47 +0800

    firmware_class: change the firmware loading timeout time to 3 secs.

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -90,7 +90,7 @@ enum {
 	FW_STATUS_ABORT,
 };
 
-static int loading_timeout = 60;	/* In seconds */
+static int loading_timeout = 3;	/* In seconds */
 
 static inline long firmware_loading_timeout(void)
 {
