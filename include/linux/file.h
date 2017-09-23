c039d987b7fc4af51729a368a1ce6cfc60e3317d
YuanCheng Cheng <onlywig@gmail.com>
2017-09-24 00:20:22 +0800

    aufs4: add aufs file system support.
    it's refer from sfjro aufs-4.13+ branch on github
    
    Signed-off-by: YuanCheng Cheng <onlywig@gmail.com>

--- include/linux/file.h
+++ include/linux/file.h
@@ -20,6 +20,8 @@ struct path;
 extern struct file *alloc_file(struct path *, fmode_t mode,
 	const struct file_operations *fop);
 
+extern struct file *get_empty_filp(void);
+
 static inline void fput_light(struct file *file, int fput_needed)
 {
 	if (fput_needed)
